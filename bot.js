b636c958e04a1c8614c3429ae3860fbf9460fde4
aziz127182 <32429099+aziz127182@users.noreply.github.com>
2018-01-05 13:05:03 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,15 +1,7 @@
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 const client = new Discord.Client();
-
 client.on('ready', () => {
-    console.log('I am ready!');
+  console.log(`Logged in as ${client.user.tag} !`);
+  client.user.setGame(`Hi Im Streaming !`,`www.twitch.tv/v5bz`);
 });
-
-client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
-  	}
-});
-
-// THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
